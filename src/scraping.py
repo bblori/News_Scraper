ca7c4b5b391b32a36bb0f25585d606ca6c1c03b4
birton <birton.csalad@gmail.com>
2023-02-17 15:44:16 +0100

    Functions

--- src/scraping.py
+++ src/scraping.py
@@ -3,26 +3,31 @@ from bs4 import BeautifulSoup
 
 website = 'https://index.hu'
 
-result = requests.get(website)
-content = result.text
-soup = BeautifulSoup(content, 'lxml')
-
-# print(soup.prettify())
-cikkek = soup.find_all('h2', attrs={'class':'cikkcim'})
-
 # Kereset kulcsszavak
-keywordsList = ["Otp"]
-count = 0
-maxArticles = 5
+keywordsList = ["Orbán", "forint", "Szijjártó"]
+maxArticles = 10
 
+def index(website, keywordsList, maxArticles):
+    result = requests.get(website)
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+    cikkek = soup.find_all('h2', attrs={'class':'cikkcim'})
+    otpChecker(cikkek, keywordsList, maxArticles)
 
-for link in cikkek:
-    if count <= maxArticles:
-        cikkCim = link.find('a').get_text()
-        cikkLink = link.find('a').get('href')
-        print("-------------------")
-    else:
-        break
-    count = count + 1
+def otpChecker(cikkek, keywordsList, maxArticles):
+    count = 0
+    for link in cikkek:
+        if count <= maxArticles:
+            for key in keywordsList:
+                if key in link.find('a').get_text():
+                    cikkCim = link.find('a').get_text()
+                    cikkLink = link.find('a').get('href')
+                    print(cikkCim)
+                    print(cikkLink)
+                    print("-------------------")
+        else:
+            print("--Meghivás más honlapon is.--")
+            break
+        count = count + 1
 
-
+index(website, keywordsList, maxArticles)
