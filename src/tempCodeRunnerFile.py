2f5459863dc0c74edde8def7a325cf40c112ee4f
birton <birton.csalad@gmail.com>
2023-02-18 20:04:56 +0100

    Delete files

--- src/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-""" def index(website, keywordsList, maxArticles):
-    result = requests.get(website)
-    content = result.text
-    soup = BeautifulSoup(content, 'lxml')
-    cikkek = soup.find_all('h2', attrs={'class':'cikkcim'})
-    otpChecker(cikkek, keywordsList, maxArticles) """
